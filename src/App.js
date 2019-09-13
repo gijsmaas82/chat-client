a9317899dacfc6ca2069d9313f4541ca171b44b9
gijsmaas82 <gijsmaas82@outlook.com>
2019-09-13 11:38:05 +0200

    first set-up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import request from 'superagent'
 
-function App() {
+class App extends React.Component {
+  state ={ text: ''}
+  
+  source = new EventSource(
+    'http://localhost:4000/stream'
+    )
+  
+  componentDidMount() {
+    this.source.onmessage = (event) => {
+      const { data } = event
+      console.log('data:', data)
+
+      const messages = JSON.parse(data)
+      console.log(
+        'messages test:', messages
+      )
+    }
+  }
+
+  onChange = (event) => {
+    const { target: { value } } = event
+    console.log('value test:', value)
+
+    this.setState({ text: value })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+
+    const { text } = this.state
+
+    request
+      .post('http://localhost:4000/message')
+      .send( { text })
+      .then(console.log)
+      .catch(console.error)
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={this.onSubmit}>
+        <input 
+        type='text'
+        onChange={this.onChange}
+        value={this.state.text} />
+        <button > send </button>
+      </form>
     </div>
   );
+  }
 }
 
 export default App;
